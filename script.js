4c822c3be5899fbe053f9988f532c992b6560178
Diyorbek G'aybullayev <gaybullayevdiyorbek2001@gmail.com>
2023-10-25 18:02:05 +0500

    bootslander sayti

---
+++ script.js
@@ -0,0 +1,35 @@
+// let faqh6 =document.querySelector(".faq-h6")
+// let faqlabel =document.querySelector(".faq-label")
+
+// function faqbos() {
+//     if (true) {
+//         faqh6.style.display="block" 
+//         faqlabel.style.color=" #1acc8d"
+//     }
+//     else if (false) {
+//         faqh6.style.display="none" 
+//         faqlabel.style.color=" #000264"
+//     }
+
+//     // event.defaultPrevented();
+// }
+
+let name111 =document.querySelector(".name1")
+let email111 =document.querySelector(".email1")
+let text111 =document.querySelector(".text1")
+let textarea111 =document.querySelector(".textarea1")
+
+function contactboss() {
+    name111.style="border: 2px solid #1acc8d;"
+    email111.style="border: 2px solid #1acc8d;"
+    text111.style="border: 2px solid #1acc8d;"
+    textarea111.style="border: 2px solid #1acc8d;"
+
+}
+
+
+
+
+
+
+
